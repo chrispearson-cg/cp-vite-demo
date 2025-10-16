1bb117dca3d22698aa81bf01ae5480858f948135
Chris Pearson <chris.pearson@connellsgroup.co.uk>
2025-10-16 15:10:29 +0100

    feat: add banner to main page
    Use scss for styling

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+﻿import logo from '../assets/prism.png';
+import logoClass from './Banner.module.scss';
+
+const Banner = () => {
+    return (
+        <>
+        <header>
+            <img src={logo} alt="Prism Logo" className={logoClass.logo}/>
+        </header>
+        </>
+    );
+}
+
+export default Banner
