fd7e7bea1d1bbf68cd2c1f46a97855d0f657fc11
Chris Pearson <chris.pearson@connellsgroup.co.uk>
2025-10-16 16:28:14 +0100

    feat: persist theme choice to local storage

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,15 +1,27 @@
 ﻿import { useEffect, useState } from "react";
 
-const light= "light";
-const dark= "dark";
+const light = "light";
+const dark = "dark";
+const themeKey = "theme"
+
+const getStoredTheme = () => localStorage.getItem(themeKey)
+const setStoredTheme = (theme: string) => localStorage.setItem(themeKey, theme)
 
 const ThemeToggle = () => {
+    const [theme, setTheme] = useState(light);
 
     function toggleTheme() {
-        setTheme((prevTheme) => (prevTheme === light ? dark : light));
+        const newTheme = theme === light ? dark : light;
+        setStoredTheme(newTheme);
+        setTheme(newTheme);
     }
 
-    const [theme, setTheme] = useState(light);
+    useEffect(() => {
+        const savedTheme = getStoredTheme();
+        if (savedTheme) {
+            setTheme(savedTheme);
+        }
+    }, []);
 
     useEffect(() => {
         document.body.setAttribute('data-bs-theme', theme);
